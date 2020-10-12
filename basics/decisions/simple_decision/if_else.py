ede8dacbcffbfa0abb2ef656f61331050f758e01
repl.it user <replituser@example.com>
2020-10-12 12:17:40 +0000

    Created an if/else program

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,14 @@
+# User input for activity
+print("Enter an activity:")
+activity = input()
+
+# Message output if activity is to calculate
+if (activity == "calculate"):
+  print("\nPerforming calculations...")
+
+# Message output if activity is anything other than to calculate
+else:
+  print("\nPerforming activity...")
+
+# Message output to display completion of activity
+print("\n Activity completed!")
