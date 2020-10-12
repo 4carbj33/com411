15390ac52790ae68fe4a2b2be77bde9c6070072e
repl.it user <replituser@example.com>
2020-10-12 13:54:40 +0000

    Created programs that use modulo operators and comparison operators.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,12 @@
+# User input for numbers to compare
+first = int(input("Please enter the first number: "))
+
+second = int(input("Please enter the second number: "))
+
+# Message output if first is greater
+if first > second:
+  print("The second number is the smallest.")
+
+# Message output if second is greater
+else:
+  print("The first number is smallest.")
