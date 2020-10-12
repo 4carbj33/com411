318ec3cabb225e67f7da3b5e5c31ac72ead33df6
repl.it user <replituser@example.com>
2020-10-12 12:06:58 +0000

    Created new working folder for if statements

---
+++ basics/decisions/simple_decision/if.py
@@ -0,0 +1,10 @@
+# User input for book type
+print("What type of book is this?")
+type = input()
+
+# Adventure book answer
+if (type == "adventure"):
+  print("\nI like adventure books!")
+
+# Message output
+print("\n Finished reading book.")
