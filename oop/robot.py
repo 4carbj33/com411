d9abc15dcefe31ae0e4fe6ebfda134c2c4ef59cf
repl.it user <replituser@example.com>
2020-12-03 13:32:28 +0000

    Updated classes human and robot with instance methods.

--- oop/robot.py
+++ oop/robot.py
@@ -10,14 +10,35 @@ class Robot:
     self.name = "Robot"
     self.age = 0
     self.energy = 0
-
+  
   # An instance method
   def display(self):
     print(f"I am {self.name}")
 
+  def eat(amount):
+    if (self.energy + amount > Robot.MAX_ENERGY):
+      self.energy = Robot.MAX_ENERGY
+    else:
+      self.energy += amount
+
+  def move(distance):
+    if (self.energy - distance < 0):
+      self.energy = 0
+    else:
+      self.energy -= distance
+
+  def __repr__(self):
+    return f'robot(name={self.name}, age={self.age})'
+  
+  def __str__(self):
+   return f'My name is {self.__name} and I am {self.age} years old.' 
+
 if (__name__ == "__main__"):
   robot = Robot()
   robot.display()
+
+  print(robot)
+  print(repr(robot))
 
 
 
